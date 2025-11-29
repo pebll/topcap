9755614bf56660cd0c8bfdf271f0ab7ae4cd0152
Léo <leo@brucker.fr>
2025-11-29 23:41:44 +0100

    Add neighbourCount and overload getSetClearBit functions with coords signature

--- cpp/include/bitboard.h
+++ cpp/include/bitboard.h
@@ -16,12 +16,18 @@ inline bool operator==(const Coordinates &lhs, const Coordinates &rhs) {
   return lhs.x == rhs.x && lhs.x == rhs.x;
 }
 
+// bitboard operations
 int getBit(Bitboard bitboard, int position);
 Bitboard setBit(Bitboard bitboard, int position);
 Bitboard clearBit(Bitboard bitboard, int position);
-
+int getBit(Bitboard bitboard, Coordinates coords, int N);
+Bitboard setBit(Bitboard bitboard, Coordinates coords, int N);
+Bitboard clearBit(Bitboard bitboard, Coordinates coords, int N);
+// coords & position operations
 int coordsToPosition(Coordinates coords, int N);
 Coordinates positionToCoords(int position, int N);
+// utility functions
+int neighbourCount(Bitboard bitboard, Coordinates coords, int N);
 
 } // namespace bitboard
 
