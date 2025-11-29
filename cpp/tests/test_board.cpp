31445a112355b45ef78df6e38c22ea5410947ae8
Léo <leo@brucker.fr>
2025-11-29 10:39:10 +0100

    Add initialBoard function with tests. setup gdp debugging

--- cpp/tests/test_board.cpp
+++ cpp/tests/test_board.cpp
@@ -2,15 +2,27 @@
 #include "catch.hpp"
 
 #define TEST_ALL
-#ifndef TEST_ALL
 
 TEST_CASE("initialBoard gives correct initial states", "[board]") {
-  Bitboard white4 = 0b0010'0001'0000'0000;
-  Bitboard black4 = 0b0000'0000'1000'0100;
-  Board board4 = {white4, black4};
-  REQUIRE(initialBoard(4) == board4);
+  // y
+  // -  -  B  -  -
+  // -  -  -  B  -
+  // W  -  -  -  B
+  // -  W  -  -  -
+  // -  -  W  -  -  x
+  //
+  Bitboard white4 = 0b0000'0000'0001'0010;
+  Bitboard black4 = 0b0100'1000'0000'0000;
+  REQUIRE(initialBoard(4).white == white4);
+  REQUIRE(initialBoard(4).black == black4);
+
+  Bitboard white5 = 0b00000'00000'00001'00010'00100;
+  Bitboard black5 = 0b00100'01000'10000'00000'00000;
+  REQUIRE(initialBoard(5).white == white5);
+  REQUIRE(initialBoard(5).black == black5);
 }
 
+#ifndef TEST_ALL
 TEST_CASE("printBoard prints correct string", "[board]") {
   // Test multiple sizes
   REQUIRE(true);
