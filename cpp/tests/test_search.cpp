5550597fab9bbff787516f761c9f035d2749c0b1
Léo <leo@brucker.fr>
2025-12-05 17:55:19 +0100

    fix test

--- cpp/tests/test_search.cpp
+++ cpp/tests/test_search.cpp
@@ -27,13 +27,13 @@ TEST_CASE("White Minimax returns mate in 1", "[board]") {
 // Board of size 4
 //         a b c d
 //       4 · · · · 4
-//       3 · · ● ·  3
+//       3 · · · ● 3
 //       2 ○ ○ ● ·  2
 //       1 · · · · 1
 //         a b c d
 //
 TEST_CASE("black Minimax returns mate in 1", "[board]") {
-  bitboard::Bitboard white = 0b0000'0100'0100'0000;
+  bitboard::Bitboard white = 0b0000'1000'0100'0000;
   bitboard::Bitboard black = 0b0000'0000'0011'0000;
   Board board = {white, black, 4, false};
   Move expectedMove = {{0, 1}, {0, 0}};
